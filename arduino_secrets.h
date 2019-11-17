48d9609f88bb718749ce0352113f3603fbb50865
VictorMoraJimenez <36267572+VictorMoraJimenez@users.noreply.github.com>
2019-11-17 23:44:14 +0100

    Add device, node and property functions

---
+++ arduino_secrets.h
@@ -0,0 +1,19 @@
+//Wifi config
+#define SECRET_SSID "WrpiVictorMora"
+#define SECRET_PASS "Mirvic11."
+//MQTT config
+#define SECRET_MQTT_HOST "192.168.1.52"
+#define SECRET_MQTT_USER "vmoraji"
+#define SECRET_MQTT_PASS "Curs-IoT.19"
+#define SECRET_MQTT_ID "device001"
+//Homie device config
+#define DEVICE_HOMIE "4.0.0"
+#define DEVICE_NAME "Device 1"
+#define DEVICE_NODES "meteo001"
+#define DEVICE_IMPLEMENTATION "mkr1000"
+#define DEVICE_STATE "ready"
+//Homie node config
+#define NODE_ID "meteo001"
+#define NODE_NAME "meteo 1"
+#define NODE_TYPE "mkrenv"
+#define NODE_PROPERTIES "temperature,humidity,pressure,uva,uvb,uvindex"
